056a117dfcd6f838bbd24cd6a169bdaac4aaf044
Udo Borkowski <ub@abego-software.de>
2021-11-06 18:39:30 +0100

    create copy of sample classes, as base for "with extensions" tests

---
+++ src/test/java/org/abego/lab/methodextension/withext/WithExtensionsTest.java
@@ -0,0 +1,90 @@
+package org.abego.lab.methodextension.withext;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class WithExtensionsTest {
+    @Test
+    void B_m2() {
+        StringBuilder sb = new StringBuilder();
+
+        new B().m2(sb);
+
+        assertEquals("" +
+                "B.m2\n" +
+                "A.m1\n", sb.toString());
+    }
+
+    @Test
+    void C_m2() {
+        StringBuilder sb = new StringBuilder();
+
+        new C().m2(sb);
+
+        assertEquals("" +
+                "C.m2\n" +
+                "B.m2\n" +
+                "A.m1\n" +
+                "A.m1b\n", sb.toString());
+    }
+
+    @Test
+    void D_m2() {
+        StringBuilder sb = new StringBuilder();
+
+        new D().m2(sb);
+
+        assertEquals("" +
+                "D.m2\n" +
+                "C.m2\n" +
+                "B.m2\n" +
+                "D.m1\n" +
+                "A.m1b\n", sb.toString());
+    }
+
+    @Test
+    void E_m2() {
+        StringBuilder sb = new StringBuilder();
+
+        new E().m2(sb);
+
+        assertEquals("" +
+                "E.m2\n" +
+                "D.m2\n" +
+                "C.m2\n" +
+                "B.m2\n" +
+                "E.m1\n" +
+                "E.m1b\n", sb.toString());
+    }
+    @Test
+    void D_m3() {
+        StringBuilder sb = new StringBuilder();
+
+        new D().m3(sb);
+
+        assertEquals("" +
+                "D.m3\n" +
+                "D.m2\n" +
+                "C.m2\n" +
+                "B.m2\n" +
+                "D.m1\n" +
+                "A.m1b\n", sb.toString());
+    }
+
+    @Test
+    void E_m3() {
+        StringBuilder sb = new StringBuilder();
+
+        new E().m3(sb);
+
+        assertEquals("" +
+                "D.m3\n" +
+                "E.m2\n" +
+                "D.m2\n" +
+                "C.m2\n" +
+                "B.m2\n" +
+                "E.m1\n" +
+                "E.m1b\n", sb.toString());
+    }
+}
